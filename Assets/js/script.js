62119f972cf18423c54431331034393add2fd012
johnrip89 <lebronjohn89@gmail.com>
2021-11-04 11:13:05 -0600

    add time blocks and added function to the save button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,13 @@
 var saveBtn = document.querySelector('.saveBtn');
 var event = document.querySelector('.event');
 
-$(currentDay).text(moment().format("[Today is] dddd, MMMM Do YYYY")); 
+$("#currentDay").text(moment().format("[Today is] dddd, MMMM Do YYYY"));
 
+$(".saveBtn").click(function () {
+    console.log($(this));
+    console.log($(".saveBtn"));
+    var time = $(this).parent().attr("id");
+    var textarea = $(this).siblings(".textarea").val();
+    localStorage.setItem(time, textarea);
+})
 
